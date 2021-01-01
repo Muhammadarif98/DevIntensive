b4ca243c9b521e72d6d4a25e665a4e16907a308f
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2021-01-02 01:42:51 +0300

    commit

--- app/src/main/java/com/example/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/devintensive/ui/activities/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.devintensive.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.devintensive.R;
 import com.example.devintensive.utils.ConstantManager;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -19,6 +25,8 @@ public class MainActivity extends BaseActivity {
     private static final String TAG = ConstantManager.TAG_PREFIX + "MainActivity";
     private ImageView mCallImg;
     private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
 
 
 
@@ -29,14 +37,18 @@ public class MainActivity extends BaseActivity {
         Log.d(TAG,"OnCreate");
 
         mCallImg = findViewById(R.id.call_img);
-        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mCoordinatorLayout = findViewById(R.id.main_coordinator_container);
+        mToolbar = findViewById(R.id.toolbar);
+        mDrawerLayout = findViewById(R.id.navigation_drawer);
+        setupToolBar();
+        setupDrawer();
 
         if (savedInstanceState == null){
-            showSnackbar("активити запускается впервые");
-            showToast("активити запускается впервые");
+//            showSnackbar("активити запускается впервые");
+//            showToast("активити запускается впервые");
         }else {
-            showSnackbar("активити уже создавалось");
-            showToast("активити уже создавалось");
+//            showSnackbar("активити уже создавалось");
+//            showToast("активити уже создавалось");
         }
 
         mCallImg.setOnClickListener(new View.OnClickListener() {
@@ -48,19 +60,36 @@ public class MainActivity extends BaseActivity {
         });
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void showSnackbar(String message){
         Snackbar.make(mCoordinatorLayout,message,Snackbar.LENGTH_LONG).show();
     }
+    private void setupToolBar(){
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    private void setupDrawer(){
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
 
-//    private void runWithDelay(){
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//            //TODO: Выполнить с задержкой
-//                hideProgress();
-//
-//            }
-//        },5000);
-//    }
 }
